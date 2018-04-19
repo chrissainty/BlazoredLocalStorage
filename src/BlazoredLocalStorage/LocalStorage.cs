82a401e32be3decd8625c743fd678572c6057295
Chris Sainty <chrissainty@users.noreply.github.com>
2018-04-19 07:42:24 +0100

    Initial project

---
+++ src/BlazoredLocalStorage/LocalStorage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Browser.Interop;
+
+namespace Blazored.Js
+{
+    public static class LocalStorage
+    {
+        public static void Save(string identifier, object data)
+        {
+            var serialisedData = JsonUtil.Serialize(data);
+            RegisteredFunction.Invoke<bool>("Blazored.Js.LocalStorage.Save", identifier, serialisedData);
+        }
+
+        public static T Get<T>(string identifier)
+        {
+            var serialisedData = RegisteredFunction.Invoke<string>("Blazored.Js.LocalStorage.Get", identifier);
+            
+            if (serialisedData == null)
+                return default(T);
+            
+            return JsonUtil.Deserialize<T>(serialisedData);
+        }
+
+        public static void Remove(string identifier)
+        {
+            RegisteredFunction.Invoke<string>("Blazored.Js.LocalStorage.Remove", identifier);
+        }
+    }
+}
---
+++ src/BlazoredLocalStorage/content/blazoredLocalStorage.js
@@ -0,0 +1,12 @@
+﻿Blazor.registerFunction('Blazored.Js.LocalStorage.Save', function (identifier, data) {
+    localStorage.setItem(identifier, data);
+    return true;
+});
+
+Blazor.registerFunction('Blazored.Js.LocalStorage.Get', function (identifier) {
+    return localStorage.getItem(identifier);
+});
+
+Blazor.registerFunction('Blazored.Js.LocalStorage.Remove', function (identifier) {
+    localStorage.removeItem(identifier); return true;
+});
